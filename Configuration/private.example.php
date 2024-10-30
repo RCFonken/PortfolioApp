d9201180b6269d2fb52f80ca0e424df6d3dbb3cf
Renee Fonken <Pretletter03@gmail.com>
2024-10-30 17:57:20 +0100

    Create private.example.php

---
+++ Configuration/private.example.php
@@ -0,0 +1,14 @@
+<?php // private example php code
+
+return [
+  "database" => [
+    "host" => "localhost",
+    "username" => "root",
+    "password" => "",
+    "dbname" => "portfolioapp"
+  ],
+  "smtp" => [
+
+  ],
+];
+?>
